3363a522f8b46d0642de1fe011d2a955d85f62f2
harshit074 <harshit.agrawal@appdirect.com>
2020-06-21 16:01:40 +0530

    Initial Commit for Frontend with project setup

---
+++ application/src/main/java/com/frontendproject/FrontendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.frontendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendProjectApplication.class, args);
+	}
+
+}
